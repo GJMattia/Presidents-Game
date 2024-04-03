4d226db6db79d98b36ee114675d211ba82cf239b
Greg Mattia <gjmattia@gmail.com>
2024-04-03 19:36:03 -0400

    made number thing work

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,11 +5,11 @@ import { useState } from 'react';
 
 export default function App() {
 
-  const [game, setGame] = useState<boolean>(false);
+  const [game, setGame] = useState<boolean>(true);
 
   return (
     <div className='App'>
-      {!game && <MainMenu setGame={setGame} />}
+      {/* {!game && <MainMenu setGame={setGame} />} */}
       {game && <Game setGame={setGame} />}
     </div>
   )
--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -9,34 +9,51 @@ interface GameProps {
 
 export default function Game({ setGame }: GameProps) {
 
-    function check(index: number): void {
+
+    //State Variables
+    const [numbers, setNumbers] = useState<number[]>(Array.from({ length: 46 }, (_, index: number) => index))
+    const [current, setCurrent] = useState<number>(randomNumber(numbers));
+
+    //regular variables
+    const presidents: number[] = new Array(46).fill(null);
+
+    //Functions
+
+    function randomNumber(numbersArray: number[]): number {
+        const randomIndex: number = Math.floor(Math.random() * numbersArray.length);
+        return numbersArray[randomIndex];
+    }
+
+
+    function check(event: any, index: number): void {
         if (index === current) {
-            console.log('Match');
-            setCurrent(Math.floor(Math.random() * 46) + 1);
+            const updatedNumbers = numbers.filter(num => num !== current);
+            setNumbers(updatedNumbers);
+
+            setCurrent(randomNumber(updatedNumbers));
+
+            (event.target as HTMLElement).id = "Done";
         } else {
             console.log('No Match');
         }
-    };
+    }
 
-
-
-    const [current, setCurrent] = useState<number>(1);
-    const presidents: number[] = new Array(46).fill(null);
 
     return (
         <div className='Game'>
 
             <div className='Spotlight'>
                 <h1>{PresidentData[current].name}</h1>
-                <h1>{current + 1}</h1>
+                {/* <h1>{current + 1}</h1> */}
+
             </div>
 
             <div className='Presidents'>
                 {presidents.map((_, index) => (
                     <div
-                        onClick={() => check(index)}
+                        onClick={(event) => check(event, index)}
                         key={index} className="PresidentDiv">{index + 1}
-                        {PresidentData[index].name}
+
                     </div>
                 ))}
             </div>
